975cf4b4aa7e051a594a4246b09d2f4049122bc6
Damien Hobday <damienhobday@gmail.com>
2016-10-20 22:23:35 -0400

    Renamed test.py in yahoo folder to query parser. Query parsing partially achieved.

---
+++ yahoo_finance_tests/query_parser.py
@@ -0,0 +1,25 @@
+from yahoo_finance import Share
+
+example_string = 'How much was traded in AAPL?'
+
+def stripper(string): #Function to strip punctuation from string
+
+    return string.replace(',',''
+                ).replace('.',''
+                ).replace('?',''
+                ).replace('!',''
+                )
+
+def splitter(string):
+    string_list = string.split()
+    return string_list
+
+def search_for_symbol(string):
+    stripped_string = stripper(string)
+    split_string = splitter(stripped_string)
+    stock_symbol = ''
+
+    for word in split_string:
+        if word.isupper():
+            stock_symbol = word
+            return stock_symbol
