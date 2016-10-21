37769bb92d011738443d12b1ac713b034e2fec0a
Damien Hobday <damienhobday@gmail.com>
2016-10-21 00:33:04 -0400

    Price and closing price working.

--- query_parsing/query_info.py
+++ query_parsing/query_info.py
@@ -1,7 +1,8 @@
 from yahoo_finance import Share
 from query_parser import *
+import re
 
-search_string = 'How much was traded in AAPL?'
+search_string = 'GPRO?'
 
 #print(search_for_symbol(search_string))
 
@@ -9,14 +10,21 @@ def price_return(string):
     symbol = search_for_symbol(string)
     return Share(symbol).get_price()
 
+def previous_closing_price(string):
+    symbol = search_for_symbol(string)
+    return Share(symbol).get_prev_close()
 
 def what_info(string):
-    if 'how much' in search_string.lower():
-        return price_return(string)
+    results = []
+    if re.search('how.*much|price', search_string.lower()):
+        results.append('Price: ' + price_return(string))
+    if re.search('(closing.* price)|(yesterday.*price)|(yesterday)', search_string.lower()):
+        results.append('Yesterday\'s Closing Price: ' + previous_closing_price(string))
+    return results
 
 
 def key_words(string):
     word_list = splitter(stripper(string))
     return word_list
 
-print (what_info(search_string))
+print(what_info(search_string))
