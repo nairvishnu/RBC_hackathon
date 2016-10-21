6a9d6fa708d7f08862d27e8f0b633b790892f836
Vishnu Nair <vishnu4nair@gmail.com>
2016-10-20 20:56:23 -0400

    Removing ZC Starter Kit boilerplate.

--- Zahn-Center-Hackathon-Starter-Kit-master/django_boilerplate/example/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-
-# Example:
-# class Foo(models.Model):
-#     id = models.AutoField(primary_key=True)
