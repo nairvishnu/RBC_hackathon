3342a2061cd86ff5bae526371c69528f6efbfb54
Vishnu Nair <vishnu4nair@gmail.com>
2016-10-21 07:33:43 -0400

    Fixing unsuccessful merge conflict resolutions.

--- pluto/pluto_app/views.py
+++ pluto/pluto_app/views.py
@@ -7,15 +7,8 @@ def index():
     all_args = request.args.to_dict()
     try:
         search_string = all_args['search']
-<<<<<<< HEAD
         the_info = query_info.what_info(search_string)
         the_info_list = [item for sublist in the_info for item in sublist] #list comprehension here to flatten list.
         return render_template('index.html', title="Home", search_param=search_string, mytest=the_info_list)
-=======
-        if search_string == "":
-            return render_template('index.html', title="Home")
-        else:
-            return render_template('index.html', title="Home", search_param=search_string)
->>>>>>> upstream/master
     except KeyError:
         return render_template('index.html', title="Home")
