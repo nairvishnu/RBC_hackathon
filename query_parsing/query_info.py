4792407023c4bf071a575206974cbb89359d1483
Damien Hobday <damienhobday@gmail.com>
2016-10-21 02:46:59 -0400

    Lots of stock data.

--- query_parsing/query_info.py
+++ query_parsing/query_info.py
@@ -2,7 +2,7 @@ from yahoo_finance import Share
 from query_parser import *
 import re
 
-search_string = 'AAPL?'
+search_string = 'dividend AAPL?'
 
 #print(search_for_symbol(search_string))
 
@@ -14,17 +14,38 @@ def previous_closing_price(string):
     symbol = search_for_symbol(string)
     return Share(symbol).get_prev_close()
 
-def stock_info(string):
+def historical_data(string):
     symbol = search_for_symbol(string)
-    return Share(symbol).get_info()
+    word_list = string.split()
+    start_date = word_list[1]
+    end_date = word_list[2]
+    return Share(symbol).get_historical(start_date, end_date)
+
+def market_cap(string):
+    symbol = search_for_symbol(string)
+    return Share(symbol).get_market_cap()
+
+def dividend_yield(string):
+    symbol = search_for_symbol(string)
+    return Share(symbol).get_dividend_yield()
+
+def dividend_share(string):
+    symbol = search_for_symbol(string)
+    return Share(symbol).get_dividend_share()
+
+def earnings_share(string):
+    symbol = search_for_symbol(string)
+    return Share(symbol).get_earnings_share()
 
 def what_info(string):
     results = []
-    if re.search('how.*much|price', search_string.lower()):
-        results.append('Price: ' + price_return(string))
     if re.search('(closing.* price)|(yesterday.*price)|(yesterday)', search_string.lower()):
         results.append('Yesterday\'s Closing Price: ' + previous_closing_price(string))
-    if stripper(search_string).isupper:
+
+    if re.search('how.*much|price', search_string.lower()):
+        results.append('Price: ' + price_return(string))
+
+    if stripper(search_string).isupper():
         symbol = search_for_symbol(string)
         local_list = []
         local_list.append('Price: ' + Share(symbol).get_price())
@@ -33,6 +54,17 @@ def what_info(string):
         local_list.append('Days High: ' + Share(symbol).get_days_high())
         local_list.append('Days Low: ' + Share(symbol).get_days_low())
         results.append(local_list)
+
+    if re.search('market.*cap', search_string.lower()):
+        results.append('Market Cap: ' + market_cap(string))
+
+    if re.search('dividend', search_string.lower()):
+        results.append('Dividend Yield: ' + dividend_yield(string))
+        results.append('Dividend Share: ' + dividend_share(string))
+
+
+    if string.split()[0] == 'history':
+        return historical_data(string)
     return results
 
 
@@ -41,3 +73,4 @@ def key_words(string):
     return word_list
 
 print(what_info(search_string))
+#print(historical_data(search_string))
